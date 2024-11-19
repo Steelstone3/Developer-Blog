f1d567e94bcc475371ed454adef21d029aa15015
Skippy <steelstone3@gmail.com>
2024-11-19 01:42:42 +0000

    version-19-11-24

--- business/src/models/blog_store.rs
+++
@@ -1,9 +0,0 @@
-use std::{collections::HashMap, sync::atomic::AtomicUsize};
-
-use super::id_blog_post::IdBlogPost;
-
-#[derive(Default)]
-pub struct BlogPostStore {
-    pub id: AtomicUsize,
-    pub store: HashMap<usize, IdBlogPost>,
-}
