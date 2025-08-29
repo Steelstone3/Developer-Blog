26562a45481b22c5d538aa283d42f6ecdbb8057b
Skippy <steelstone3@gmail.com>
2025-08-29 20:00:31 +0100

    update

---
+++ Server/Models/BlogPost.cs
@@ -0,0 +1,9 @@
+namespace Server.Models
+{
+    public class BlogPost
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
---
+++ Server/Program2.cs
@@ -0,0 +1,17 @@
+// using Microsoft.AspNetCore.Builder;
+// using Microsoft.Extensions.DependencyInjection;
+
+// WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+// builder.Services.AddControllers();
+
+// builder.Services.AddAutoMapper(cfg => 
+// {
+//     cfg.CreateMap<BlogPost, BlogPostDto>();
+// });
+
+// var app = builder.Build();
+
+// app.MapControllers();
+
+// app.Run();
